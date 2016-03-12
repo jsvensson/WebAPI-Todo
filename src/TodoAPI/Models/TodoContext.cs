e4dc6a09f84635cacbea98f1f94eaf5983cfc4c1
Johan Svensson <johan@atomicplayboy.net>
2016-03-12 18:39:20 +0100

    Declare connection string in context

--- src/TodoAPI/Models/TodoContext.cs
+++ src/TodoAPI/Models/TodoContext.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Data.Entity;
+using Microsoft.Data.Entity.Infrastructure;
 
 namespace TodoAPI.Models
 {
     public class TodoContext : DbContext
     {
+        const string connection = @"Data Source=localhost;Initial Catalog=TodoApp;Integrated Security=True";
+
         public DbSet<TodoItem> TodoItems { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connection);
+        }
     }
 }
