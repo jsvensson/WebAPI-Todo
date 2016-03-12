a67f7d15e7f16e6ed857c497abc3e2781a5e6d95
Johan Svensson <johan@atomicplayboy.net>
2016-03-12 17:46:23 +0100

    Add Entity Framework components

--- src/TodoAPI/Startup.cs
+++ src/TodoAPI/Startup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Hosting;
+using Microsoft.Data.Entity;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -30,6 +31,12 @@ namespace TodoAPI
             // Add framework services.
             services.AddMvc();
             services.AddSingleton<ITodoRepository, TodoRepository>();
+
+            // Register Entity Framework
+            var connection = @"Data Source=localhost;Initial Catalog=TodoApp;Integrated Security=True";
+            services.AddEntityFramework()
+                .AddSqlServer()
+                .AddDbContext<TodoContext>(options => options.UseSqlServer(connection));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
