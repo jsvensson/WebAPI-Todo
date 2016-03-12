b335fe9db772c0cdc2312f86ab86490c368eef84
Johan Svensson <johan@atomicplayboy.net>
2016-03-12 16:19:41 +0100

    Initial commit

---
+++ src/TodoAPI/TodoItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoAPI
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool IsCompleted { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime FinishedAt { get; set; }
+    }
+}
