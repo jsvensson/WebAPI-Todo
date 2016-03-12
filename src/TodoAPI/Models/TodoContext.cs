a7ff1f570887f7cfdbfaf72ef8e9211dacbf84ce
Johan Svensson <johan@atomicplayboy.net>
2016-03-12 17:55:24 +0100

    Create EF context for TodoItem

---
+++ src/TodoAPI/Models/TodoContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Data.Entity;
+
+namespace TodoAPI.Models
+{
+    public class TodoContext : DbContext
+    {
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
